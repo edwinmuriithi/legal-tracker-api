29eb9284e313ef23db8068fccc4e82dab78a5891
edwinmuriithi <kabuimuriithi@gmail.com>
2023-12-13 17:26:27 +0300

    Initial Commit

---
+++ src/main/java/com/example/legaltrackerapi/LegalTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.legaltrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/legaltrackerapi/LegalTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legaltrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
